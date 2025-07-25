d3dc6197d77d6e449f5f567dc37799a80b495416
pathakoti jayanth <jayanthpathakoti@gmail.com>
2025-07-25 18:56:46 +0000

    Sign up failed with operation not allowed. I see in the previous fix, wi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -21,9 +21,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-if (typeof window !== 'undefined' && window.location.hostname.includes('localhost')) {
-    auth.settings.appVerificationDisabledForTesting = true;
-}
-
-
 export { app, auth, db, storage };
